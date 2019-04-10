c47c81dfdaef6639d922be6ca995596084d52ec1
Bryan Rodriguez <bsteven1993@gmail.com>
2019-04-10 15:43:44 -0500

    config vuetify

--- src/main.js
+++ src/main.js
@@ -13,6 +13,9 @@ firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
 
+import store from '@/store';
+require('./config/vuetify');
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
@@ -20,6 +23,7 @@ new Vue({
   el: '#app',
   router,
   i18n,
+  store,
   components: { App },
   template: '<App/>'
 })
