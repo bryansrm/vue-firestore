08d360e784cf4b3d06b4e2bd500b6981100357d4
Bryan Rodriguez <bsteven1993@gmail.com>
2019-04-10 17:06:23 -0500

    new change

---
+++ src/config/firebase.example.js
@@ -0,0 +1,9 @@
+export default {
+    //Aqui van las credenciales proporcionadas desde firebase/firestore
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,5 +4,6 @@ export default {
     databaseURL: "https://vue-firestore-45f58.firebaseio.com",
     projectId: "vue-firestore-45f58",
     storageBucket: "vue-firestore-45f58.appspot.com",
-    messagingSenderId: "901511878271"
+    messagingSenderId: "901511878271",
+    prueba: 'hola'
 };
