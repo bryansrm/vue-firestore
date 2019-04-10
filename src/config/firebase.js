1baf18e6275a1e8790821fea059a2288dad7b53b
Bryan Rodriguez <bsteven1993@gmail.com>
2019-04-10 11:42:03 -0500

    config firebase/firestore

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAzhumJ1fDX1VM8z_lPQC1s9KtUd0VitDQ",
+    authDomain: "vue-firestore-45f58.firebaseapp.com",
+    databaseURL: "https://vue-firestore-45f58.firebaseio.com",
+    projectId: "vue-firestore-45f58",
+    storageBucket: "vue-firestore-45f58.appspot.com",
+    messagingSenderId: "901511878271"
+};
